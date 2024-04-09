a7b5f7d21d94f844b335a255d73e66e7560da87d
SangwonJwa <jwa4610@gmail.com>
2024-04-09 19:05:54 +0900

    Fix: views.py 변경

--- polls/views.py
+++ polls/views.py
@@ -18,7 +18,7 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        return render(request, 'polls/detail.html', {'question': question, 'error_message': '선택이 없습니다'})
+        return render(request, 'polls/detail.html', {'question': question, 'error_message': '선택이 없습니다!'})
     selected_choice.votes = F('votes') + 1
     selected_choice.save()
     return HttpResponseRedirect(reverse('polls:result', args=(question_id,)))
