f67bcd4f20490b13799140687757ffc078677bc9
SangwonJwa <jwa4610@gmail.com>
2024-04-09 19:01:15 +0900

    Feat: 뷰 html파일 작성

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,25 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    # 추가옵션 개수
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('질문 섹션', {'fields': ['question_text']}),
+        # 생성일 숨김(collapse)
+        ('생성일', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    # 보다 깔끔하게 질문과 날짜를 출력
+    list_display = ['question_text', 'pub_date', 'was_published_recently']
+    # pub_date 필드를 수정 불가능하게 설정
+    readonly_fields = ['pub_date']
+    inlines = [ChoiceInline]
+    # 생성일을 기준으로 필터 추가
+    list_filter = ['pub_date']
+    # Question과 Choice에 대한 검색 기능 추가
+    search_fields = ['question_text', 'choice__choice_text']
+
+admin.site.register(Question, QuestionAdmin)
--- polls/views.py
+++ polls/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404, HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import *
+from django.db.models import F
 
 def index(request):
-    return HttpResponse("Hello world.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'questions': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
-def some_url(requst):
-    return HttpResponse("Some url을 구현해 봤습니다.")
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {'question': question, 'error_message': '선택이 없습니다'})
+    selected_choice.votes = F('votes') + 1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:result', args=(question_id,)))
+
+def result(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/result.html', {'question': question})
 
 # Create your views here.
